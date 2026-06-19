8b65fe893ecfe2254744692214107d659ef20746
gaardhus <tobias_gaardhus@hotmail.com>
2026-06-19 12:54:50 +0200

    remove old unused scripts

--- scripts/get_ip.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ip addr show tun0 | grep -oP 'inet \K[\d.]+'
--- scripts/remove_ollama.sh
+++
@@ -1,7 +0,0 @@
-sudo systemctl stop ollama
-sudo systemctl disable ollama
-sudo rm /etc/systemd/system/ollama.service
-sudo rm "$(which ollama)"
-sudo rm -r /usr/share/ollama
-sudo userdel ollama
-sudo groupdel ollama
